ff55495e046e88e030366038d97f4c4a1a0bb58d
Likith <likithsrinath2000@gmail.com>
2021-12-16 17:14:03 +0530

    Change candidate elimination

--- candidateElimination.py
+++ candidateElimination.py
@@ -1,35 +1,31 @@
-# candidate elimination
-import csv
-
-with open("data.csv") as f:
-    csv_file = csv.reader(f)
-    data = list(csv_file)
-
-    s = data[1][:-1]
-    g = [['?' for i in range(len(s))] for j in range(len(s))]
-
-    for i in data:
-        if i[-1] == "Yes":
-            for j in range(len(s)):
-                if i[j] != s[j]:
-                    s[j] = '?'
-                    g[j][j] = '?'
-
-        elif i[-1] == "No":
-            for j in range(len(s)):
-                if i[j] != s[j]:
-                    g[j][j] = s[j]
-                else:
-                    g[j][j] = "?"
-        print("\nSteps of Candidate Elimination Algorithm", data.index(i)+1)
-        print(s)
-        print(g)
-    gh = []
-    for i in g:
-        for j in i:
-            if j != '?':
-                gh.append(i)
-                break
-    print("\nFinal specific hypothesis:\n", s)
-
-    print("\nFinal general hypothesis:\n", gh)
+dataarr = []
+with open('data.csv') as f:
+    for line in f:
+        dataarr.append(line.strip().split(','))
+rows = len(dataarr)
+cols = len(dataarr[0])
+shypo = ['0']*(cols-1)
+ghypo = [['?']*(cols-1)]
+print("Initial Specific Hypothesis is = ", shypo)
+print("Initial General Hypothesis is = ", ghypo)
+for x in range(1, rows):
+    lst = dataarr[x]
+    if lst[cols-1] == "1":
+        for i in range(0, cols-1):
+            if shypo[i] == lst[i]:
+                continue
+        shypo[i] = '?' if shypo[i] != '0' else lst[i]
+        for g in ghypo:
+            if g[i] != '?' and shypo[i] == '?':
+                ghypo.remove(g)
+    elif lst[cols-1] == "0":
+        ghypo.clear()
+        for i in range(0, cols-1):
+            if lst[i] != shypo[i] and shypo[i] != '?':
+                temp_list = ['?']*i + [shypo[i]] + (['?']*(cols-2-i))
+                if temp_list not in ghypo:
+                    ghypo.append(temp_list)
+    print("S Hypothesis after row ", x, " = ", shypo)
+    print("G Hypothesis after row ", x, " = ", ghypo)
+print("Final SHypothesis ", shypo)
+print("Final GHypothesis ", ghypo)
