081919ddf02ec3c39f0fd3a0a0c92efeef700b49
Likith <likithsrinath2000@gmail.com>
2022-02-04 16:38:59 +0530

    Shorten6

--- 6-naiveBayes.py
+++ 6-naiveBayes.py
@@ -1,40 +1,17 @@
-from sklearn.metrics import accuracy_score
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-from sklearn.naive_bayes import GaussianNB
-
-# load data from CSV
 data = pd.read_csv('6-naiveBayesData.csv')
-print("THe first 5 values of data is :\n", data.head())
+x = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
 
-# obtain Train data and Train output
-X = data.iloc[:, :-1]
-print("\nThe First 5 values of train data is\n", X.head())
-y = data.iloc[:, -1]
-print("\nThe first 5 values of Train output is\n", y.head())
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2)
 
-# Convert then in numbers
-le_outlook = LabelEncoder()
-X.Outlook = le_outlook.fit_transform(X.Outlook)
+model = GaussianNB()
+model.fit(xtrain, ytrain)
+predicted = model.predict(xtest)
 
-le_Temperature = LabelEncoder()
-X.Temperature = le_Temperature.fit_transform(X.Temperature)
-
-le_Humidity = LabelEncoder()
-X.Humidity = le_Humidity.fit_transform(X.Humidity)
-
-le_Windy = LabelEncoder()
-X.Windy = le_Windy.fit_transform(X.Windy)
-
-print("\nNow the Train data is :\n", X.head())
-
-le_PlayTennis = LabelEncoder()
-y = le_PlayTennis.fit_transform(y)
-
-print("\nNow the Train output is\n", y)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
-classifier = GaussianNB()
-classifier.fit(X_train, y_train)
-print("Accuracy is:", accuracy_score(classifier.predict(X_test), y_test))
+print("Confusion : ", metrics.confusion_matrix(predicted, ytest))
+print("Report : ", metrics.classification_report(predicted, ytest))
+print("Accuracy : ", metrics.accuracy_score(predicted, ytest))
