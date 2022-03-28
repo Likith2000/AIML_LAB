f214da7dd5a20dc86a0c4f0db7246ea9b4e029f9
Likith <likithsrinath2000@gmail.com>
2022-03-28 14:38:21 +0530

    Change 6-extra

--- 6-extra.py
+++ 6-extra.py
@@ -1,42 +1,26 @@
-import numpy as np
-from sklearn import metrics
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
 import pandas as pd
-msg = pd.read_csv('lab6.txt', names=['message', 'label'])
 
-print('The dimensions of the dataset', msg.shape)
-msg['labelnum'] = msg.label.map({'pos': 1, 'neg': 0})
-X = msg.message
-y = msg.labelnum
-print(X)
-print(y)
-xtrain, xtest, ytrain, ytest = train_test_split(X, y)
-print(xtest.shape)
-print(xtrain.shape)
-print(ytest.shape)
-print(ytrain.shape)
-count_vect = CountVectorizer()
-xtrain_dtm = count_vect.fit_transform(xtrain)
-xtest_dtm = count_vect.transform(xtest)
-print("Uniqu Labels")
-print(count_vect.get_feature_names())
-df = pd.DataFrame(xtrain_dtm.toarray(), columns=count_vect.get_feature_names())
-print(df)
-clf = MultinomialNB().fit(xtrain_dtm, ytrain)
-predicted = clf.predict(xtest_dtm)
-print("ytest")
-print(ytest)
-print("predicted")
-print(predicted)
-print('Accuracy metrics')
-print('Accuracy of the classifer is', metrics.accuracy_score(ytest, predicted))
-print('Confusion matrix')
-labels = np.unique(ytest)  # Assign concept labels
-a = metrics.confusion_matrix(ytest, predicted, labels=labels)
-cm = pd.DataFrame(a, index=labels, columns=labels)
-print(cm)
-print('Recall and Precison ')
-print('Recall', metrics.recall_score(ytest, predicted))
-print('Precison', metrics.precision_score(ytest, predicted))
+data = pd.read_csv("naivetext.csv", names=['message', 'label'])
+data['labelnum'] = data.label.map({'pos': 1, 'neg': 0})
+x = data.message
+y = data.labelnum
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+cv = CountVectorizer()
+x_train_dtm = cv.fit_transform(x_train)
+x_test_dtm = cv.transform(x_test)
+
+model = MultinomialNB()
+print(x_train_dtm.shape, y_train.shape)
+model.fit(x_train_dtm, y_train)
+pred = model.predict(x_test_dtm)
+
+print(metrics.accuracy_score(y_test, pred))
+print(metrics.precision_score(y_test, pred))
+print(metrics.recall_score(y_test, pred))
+print(metrics.confusion_matrix(y_test, pred))
+print(metrics.classification_report(y_test, pred))
